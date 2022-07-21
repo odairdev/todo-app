74dadeb68491e70d3d03a5a84c4a1d797896d25a
Odair José Custodio Junior <odair.dev@gmail.com>
2022-07-21 09:25:08 -0300

    Mob responsiviness implemented

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,32 @@
-
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import { useLogout } from '../../hooks/useLogout'
-import styles from './Header.module.css'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import { useAuth } from "../../hooks/useAuth";
+import { useLogout } from "../../hooks/useLogout";
+import styles from "./Header.module.css";
 
 export function Header() {
-  const { logout, isPending } = useLogout()
+  const { logout, isPending } = useLogout();
+  const { user } = useAuth();
 
   return (
     <header className={styles.header}>
       <nav>
         <ul>
-          <li><Link to="/login">Login</Link></li>
-          <button onClick={logout}>Sair</button>
+          {user ? (
+            <>
+              <li>
+                <span>Bem vindo, {user.displayName}</span>
+              </li>
+              <button onClick={logout}>Sair</button>
+            </>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
         </ul>
       </nav>
       <img src={logo} alt="Logo ToDo" />
     </header>
-  )
+  );
 }
